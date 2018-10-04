6af35272d73339bae97e93202dd66b1d77b05094
Cedric David <cedric.david@btopenworld.com>
2018-10-04 16:46:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
-
+car totalBatteries = batteryBatches.reduce(function(sum, num){ return sum + num}, 0)
 const monologueLines = [
   'Who are you talking to right now?',
   'Who is it you think you see?',
